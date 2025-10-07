7b0faae7d2bf56771879688159742be84172ac17
sydney-mtvgn <sydney.montevirgen2027@gmail.com>
2025-10-07 11:55:40 -0400

    forloop

--- Lesson6-For_Loops/forloops.py
+++ Lesson6-For_Loops/forloops.py
@@ -46,22 +46,22 @@ char = ''
 # Digit check
 '0' <= char <= '9'
 
-# Your code here
-username = input("Enter your username: ")
-has_number = False
-has_uppercase = False
-total_chars = len(username)
+# # Your code here
+# username = input("Enter your username: ")
+# has_number = False
+# has_uppercase = False
+# total_chars = len(username)
 
-for char in username:
-    if 'A' <= char <= 'Z':
-        has_uppercase = True
-    if '0' <= char <= '9':
-        has_number = True
+# for char in username:
+#     if 'A' <= char <= 'Z':
+#         has_uppercase = True
+#     if '0' <= char <= '9':
+#         has_number = True
 
-print(f"Username: {username}")
-print(f"Total characters: {total_chars}")
-print(f"Has number: {has_number}")
-print(f"Has uppercase: {has_uppercase}")
+# print(f"Username: {username}")
+# print(f"Total characters: {total_chars}")
+# print(f"Has number: {has_number}")
+# print(f"Has uppercase: {has_uppercase}")
 
 
 
@@ -72,9 +72,15 @@ print(f"Has uppercase: {has_uppercase}")
 # Count how many times the letter 'e' appears (case-insensitive)
 
 tweet = "Hello everyone! Hope you're having a great day!"
-
+e = "eE"
+unique = ''
 e_count = 0
 # Your loop here
+for char in tweet:
+    if char in e:
+        if char.lower() == 'e':
+            e_count += 1
+print(f"E Count: {e_count}")        
 
 
 # PART 4: CODE ALONG - Message Repeater
@@ -88,7 +94,9 @@ message = "Study hard!"
 times = 5
 
 # Your code here
-
+print('\n ---Message Repeater---')
+for i in range(times, 0, -1):
+    print(f"{i}...{message}")
 
 # PART 5: YOUR TURN - Text Message Analyzer
 # ========================================
@@ -98,6 +106,27 @@ times = 5
 # - Number of punctuation marks (! ? . ,)
 
 text = "Hey! How are you doing today? :)"
+total_chars = len(text)
+spaces = 0
+space = " "
+for char in text:
+    if char in space:
+        spaces += 1
+letters = 0
+for char in text:
+    if 'A' <= char <= 'Z' or 'a' <= char <= 'z':
+        letters += 1
+punctuation = 0
+punc = "!.?,"
+for char in text:
+    if char in punc:
+        punctuation += 1
+
+print(f"Text: {text}")
+print(f"Total Characters: {total_chars}")
+print(f"Letters: {letters}")
+print(f"Spaces: {spaces}")
+print(f"Punctuation: {punctuation}")
 
 # Your analysis code here
 
@@ -106,10 +135,13 @@ text = "Hey! How are you doing today? :)"
 # ============================================================================
 
 # Challenge A: Print squares of numbers 1-5 (1, 4, 9, 16, 25)
-
+for i in range(1,6):
+    print (i ** 2)
 
 # Challenge B: Print countdown from 10 to 1
-
+for i in range(10,0,-1):
+    print(f"{i}...")
 
 # Challenge C: Print every 3rd number from 0 to 15 (0, 3, 6, 9, 12, 15)
-
+for i in range(0, 16, 3):
+    print(f"{i}")
