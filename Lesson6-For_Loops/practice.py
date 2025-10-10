5ae1b2cc62a617283e1ea7abeaba347a7a07b9ba
sydney-mtvgn <sydney.montevirgen2027@gmail.com>
2025-10-10 10:34:08 -0400

    practice

--- Lesson6-For_Loops/practice.py
+++ Lesson6-For_Loops/practice.py
@@ -10,3 +10,49 @@ for char in word:
     reversed_word = char + reversed_word
 
 print(reversed_word)
+
+#11
+for char in range (10,0,-1):
+    print(char)
+
+print(f"Blast off!")
+
+#12
+text = "Hello World"
+count = 0
+for char in text:
+    if char.lower() == 'o':
+        count += 1
+print(count)
+
+#13
+password = "Pass123"
+uppercase = False
+lowercase = False
+digits = False
+
+for char in password:
+    if 'A' <= char <= 'Z':
+        uppercase = True
+    elif 'a' <= char <= 'z':
+        lowercase = True
+    elif '0' <= char <= '9':
+        digits = True
+
+valid = len(password) >= 8 and uppercase and lowercase and digits
+print(f"Is a Valid Password: {valid}")
+
+#14
+sum = 0
+for char in range(1,101):
+    sum += char
+print(sum)
+
+#15
+text = "abc123xyz456"
+digits = ""
+
+for char in text:
+    if '0' <= char <= '9':
+        digits += char
+print(digits)
